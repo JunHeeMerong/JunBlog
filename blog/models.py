4800421e418cf0c130fc9b8994387ef45660e64d
이준희 <reborn37@naver.com>
2023-04-26 23:13:06 +0900

    템플릿,css 적용 및 메인페이지,블로그페이지 약식적용

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_question')  
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    click = models.IntegerField(null=True, default=0) # 클릭
+
+class Answer(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_answer')
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from django.core.paginator import Paginator
+from django.db.models import Q
+
+from .models import Post
 
 # Create your views here.
 def index(request):
     return HttpResponse("test")
+
+def bloghome(request):
+    page = request.GET.get('page', '1') # 페이지
+    kw = request.GET.get('kw', '')  # 검색어
+    post_list = Post.objects.order_by('-create_date')
+    if kw:
+        post_list = post_list.filter(
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(answer__content__icontains=kw) |  # 답변 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+        ).distinct()
+    paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'post_list': page_obj, 'page': page, 'kw': kw}
+    return render(request, 'blog/blogmain.html', context)
+
+def detail(request, post_id):
+    post = get_object_or_404(Post, pk=post_id)
+    context = {'post': post}
+    return render(request, 'main/post_detail.html', context)
