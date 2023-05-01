f64e4b1b534a16dc43a23f51fb031f13fdbf99af
이준희 <reborn37@naver.com>
2023-05-01 20:09:10 +0900

    게시글수 표시 및 기타수정

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,7 @@ from django.forms import modelformset_factory
 from django.http import HttpResponseRedirect
 from django.utils import timezone
 from django.forms import modelformset_factory
+from django.contrib.auth.models import User
 
 from .models import *
 from .forms import PostForm,ImageForm
@@ -15,6 +16,11 @@ def index(request):
     return HttpResponse("test")
 
 def bloghome(request):
+    # 유저의 포스트개수 세기
+    if request.user in User.objects.all(): # 유저가 로그인하면
+        posts = Post.objects.filter(author_id=request.user).count() # 유저의 아이디=포스트 작성자 아이디 인 포스트들의 갯수를 세서 posts에 담아서 render
+    else:
+        posts = Post.objects
     page = request.GET.get('page', '1') # 페이지
     kw = request.GET.get('kw', '')  # 검색어
     post_list = Post.objects.order_by('-create_date')
@@ -28,7 +34,7 @@ def bloghome(request):
         ).distinct()
     paginator = Paginator(post_list, 10)  # 페이지당 10개씩 보여주기
     page_obj = paginator.get_page(page)
-    context = {'post_list': page_obj, 'page': page, 'kw': kw} # ,'postForm': postForm, 'formset': formset
+    context = {'post_list': page_obj, 'page': page, 'kw': kw, 'posts':posts} # ,'postForm': postForm, 'formset': formset
     return render(request, 'blog/blogmain.html', context)
 
 def detail(request, post_id):
--- main/view.py
+++ main/view.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from blog.models import Post
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/main.html')
+    # 유저의 포스트개수 세기
+    if request.user in User.objects.all(): # 유저가 로그인하면
+        posts = Post.objects.filter(author_id=request.user).count() # 유저의 아이디=포스트 작성자 아이디 인 포스트들의 갯수를 세서 posts에 담아서 render
+    else:
+        posts = Post.objects
+    context = {'posts':posts}
+    return render(request, 'main/main.html',context)
