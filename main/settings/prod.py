d457132bee3adb94907130858b4a59dc6552cc60
이준희 <reborn37@naver.com>
2023-05-05 00:49:47 +0900

    도메인 설정

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 
 #서버용
 
-ALLOWED_HOSTS = ['43.200.120.132']
+ALLOWED_HOSTS = ['43.200.120.132','junheemerong.kr']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
