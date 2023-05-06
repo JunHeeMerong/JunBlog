011b8d842b2a819db2b4e9a6dcfc4597a1f653cf
이준희 <reborn37@naver.com>
2023-05-06 22:21:05 +0900

    nginx 미디어 파일 포기

--- main/urls.py
+++ main/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('freeboard/',include('freeboard.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+
 handler404 = 'common.views.page_not_found'
