524aa8c0001bcf3d57b74e434db4083010e3477a
이준희 <reborn37@naver.com>
2023-05-18 21:18:27 +0900

    pip추가 및 채팅공개

--- main/settings/base.py
+++ main/settings/base.py
@@ -229,3 +229,13 @@ LOGGING = {
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_IMAGE_BACKEND = "pillow" 
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
