f740e817c458dfd689beff0e157ffd5c859cd44a
이준희 <reborn37@naver.com>
2023-05-19 09:26:02 +0900

    prod.py static 변경

--- main/settings/prod.py
+++ main/settings/prod.py
@@ -4,6 +4,6 @@ import os
 #서버용
 
 ALLOWED_HOSTS = ['43.200.120.132','junheemerong.kr']
-STATIC_ROOT = BASE_DIR / 'static/'
-STATICFILES_DIRS = []
+#STATIC_ROOT = BASE_DIR / 'static/'
+#STATICFILES_DIRS = []
 DEBUG = True
